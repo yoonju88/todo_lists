523a6e5d14b358495be685b9dd700aa5687a94fd
yoonju88 <yoonju.t@gmail.com>
2025-01-14 13:35:18 +0100

    Modification nav lists

--- config/site.ts
+++ config/site.ts
@@ -9,16 +9,8 @@ export const siteConfig = {
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
+      label: "Todos",
+      href: "/todos",
     },
     {
       label: "About",
