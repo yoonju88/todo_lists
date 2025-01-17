d8566ade92ad047d8f4c55ceb82a69c73884c176
yoonju88 <yoonju.t@gmail.com>
2025-01-17 11:27:08 +0100

    check header content type

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -4,18 +4,19 @@ import TodosTable from "@/components/Table";
 
 async function fetchTodosApi() {
   const response = await fetch(`${process.env.BASE_URL}/api/todos/`, { cache: 'no-store' })
+  const contentTypeHeaderValue = response.headers.get('Content-Type')
+  if (contentTypeHeaderValue?.includes("text/html")) return null;
   return response.json()
 }
 
 export default async function TodosPage() {
-  const todoLists = await fetchTodosApi()
+  const response = await fetchTodosApi()
+  const todoLists = response?.data ?? []
 
   return (
     <div className="flex flex-col space-y-10">
       <h1 className={title()}>To Do List</h1>
-      <TodosTable
-        todos={todoLists.data ?? []}
-      />
+      <TodosTable todos={todoLists} />
     </div>
   );
 }
