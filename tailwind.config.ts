c37ff2120036545abfeb8fdac5d9c32f07b92955
yoonju88 <yoonju.t@gmail.com>
2025-01-17 13:58:39 +0100

    modi 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from '@nextui-org/theme'
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
